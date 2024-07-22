878cab4a2a90f15a39b279b8e4c50b0bf356f2c4
sopheakmen-pse <sopheak.men@pse.ngo>
2024-07-22 17:14:50 +0700

    Try deployment 2

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import memberRoutes from "./routes/memberRoutes";
 import swaggerDocs from "./swagger";
+import path from "path";
 
 const envFile = `.env.${process.env.NODE_ENV}`;
 dotenv.config({ path: envFile });
@@ -11,6 +12,7 @@ dotenv.config({ path: envFile });
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "build")));
 app.use("/auth", authRoutes);
 app.use("/api", memberRoutes);
 swaggerDocs(app);
